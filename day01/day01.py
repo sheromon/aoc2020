17b0b4e312869b7d61d7619b600892639bfa0e77
Cheryl Danner <sheromon@users.noreply.github.com>
2020-12-01 23:15:06 -0500

    Completely changed approach for day 01 to generalize for part 1 and part 2. I don't love this, but it works.

--- day01/day01.py
+++ day01/day01.py
@@ -1,22 +1,36 @@
 import numpy as np
 
 
-def day01a(input_path):
-    int_list = [int(line) for line in open(input_path)]
-    ints1 = np.array(int_list)
-    ints2 = np.copy(ints1)
-    for _ in range(len(int_list) // 2):
-        ints2 = np.append(ints2[1:], ints2[0])
-        sums = ints1 + ints2
-        match = np.where(sums == 2020)[0]
-        if match.size:
-            return ints1[match[0]] * ints2[match[0]]
+def day01(input_path, n=2):
+    # n is the number of numbers in the list that must sum to 2020
+    int_array = np.array([int(line) for line in open(input_path)])
+    initial_inds = np.array(range(n))[::-1]
+    inds = np.copy(initial_inds)
+    return recursive_check(int_array, initial_inds, inds, n - 1)
+
+
+def recursive_check(int_array, initial_inds, inds, level):
+    for _ in range(len(int_array) - initial_inds[level] - 1):
+        if level:
+            result = recursive_check(int_array, initial_inds, inds, level - 1)
+            if result:
+                return result
+        if 2020 == int_array[inds].sum():
+            return int_array[inds].prod()
+        inds[level] += 1
+    inds[level] = initial_inds[level]
 
 
 def test01a():
-    assert 514579 == day01a('test_input.txt')
+    assert 514579 == day01('test_input.txt')
+
+
+def test01b():
+    assert 241861950 == day01('test_input.txt', 3)
 
 
 if __name__ == '__main__':
     test01a()
-    print('Day 01a:', day01a('day01_input.txt'))
+    print('Day 01a:', day01('day01_input.txt'))
+    test01b()
+    print('Day 01b:', day01('day01_input.txt', 3))
