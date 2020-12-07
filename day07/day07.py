d049683498f9fcb5acca2101fe7ea021b22ad889
Cheryl Danner <sheromon@users.noreply.github.com>
2020-12-07 00:42:30 -0500

    Solutions for both parts of day07 -- for now.

--- day07/day07.py
+++ day07/day07.py
@@ -1,6 +1,13 @@
 
 
 def day07a(input_path):
+    rules_dict = get_rules_dict(input_path)
+    holders = []
+    get_holders('shiny gold', rules_dict, holders)
+    return len(holders)
+
+
+def get_rules_dict(input_path):
     rules = [line.strip() for line in open(input_path)]
     rules_dict = dict()
     leftovers = set(['s.', '.'])
@@ -20,11 +27,7 @@ def day07a(input_path):
             number, _, inner_color = bag.partition(' ')
             inner_colors[inner_color] = int(number)
         rules_dict[color] = inner_colors
-
-    holders = []
-    get_holders('shiny gold', rules_dict, holders)
-    print(holders)
-    return len(holders)
+    return rules_dict
 
 
 def get_holders(target, rules_dict, holders):
@@ -40,20 +43,30 @@ def get_holders(target, rules_dict, holders):
             done = get_holders(color, rules_dict, holders)
     return done
 
+
 def test07a():
     assert 4 == day07a('test_input.txt')
 
 
 def day07b(input_path):
-    return total
+    rules_dict = get_rules_dict(input_path)
+    return get_total_contents('shiny gold', rules_dict)
+
+
+def get_total_contents(target, rules_dict):
+    contents = rules_dict[target]
+    total_contents = 0
+    for color, quantity in contents.items():
+        total_contents += quantity * (1 + get_total_contents(color, rules_dict))
+    return total_contents
 
 
 def test07b():
-    assert 6 == day07b('test_input2.txt')
+    assert 126 == day07b('test_input2.txt')
 
 
 if __name__ == '__main__':
     test07a()
     print('Day 07a:', day07a('day07_input.txt'))
-    # test07b()
-    # print('Day 07b:', day07b('day07_input.txt'))
+    test07b()
+    print('Day 07b:', day07b('day07_input.txt'))
