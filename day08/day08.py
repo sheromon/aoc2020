d873ca94132e45dcdc75085131fdd18ed1937731
Cheryl Danner <sheromon@users.noreply.github.com>
2020-12-08 07:59:10 -0500

    Added my solution for day08b.

--- day08/day08.py
+++ day08/day08.py
@@ -4,8 +4,9 @@ class Console:
 
     def __init__(self, input_path):
         self.instructions = []
-        self.accumulator = 0
         self._read_program(input_path)
+        self.accumulator = 0
+        self.loop = False  # switches to True when revisiting an instruction
 
     def _read_program(self, input_path):
         lines = [line.strip() for line in open(input_path)]
@@ -19,7 +20,10 @@ class Console:
     def run(self):
         seen = set()
         ind = 0
-        while ind not in seen:
+        while ind < len(self.instructions):
+            if ind in seen:
+                self.loop = True
+                break
             seen.add(ind)
             action = self.instructions[ind]['action']
             value = self.instructions[ind]['value']
@@ -27,7 +31,6 @@ class Console:
                 ind += 1
             elif action == 'acc':
                 self.accumulator += value
-                # print(self.accumulator)
                 ind += 1
             elif action == 'jmp':
                 ind += value
@@ -45,16 +48,31 @@ def test08a():
 
 
 def day08b(input_path):
-    rules_dict = get_rules_dict(input_path)
-    return get_total_contents('shiny gold', rules_dict)
+    console = Console(input_path)
+    jmp_inds = [ind for ind, inst in enumerate(console.instructions) if inst['action'] == 'jmp']
+    for ind in jmp_inds:
+        console = Console(input_path)
+        console.instructions[ind]['action'] = 'nop'
+        console.run()
+        if not console.loop:
+            return console.accumulator
+
+    nop_inds = [ind for ind, inst in enumerate(console.instructions) if inst['action'] == 'nop']
+    for ind in nop_inds:
+        console = Console(input_path)
+        console.instructions[ind]['action'] = 'jmp'
+        console.run()
+        if not console.loop:
+            return console.accumulator
+    print('No solution found')
 
 
 def test08b():
-    assert 126 == day08b('test_input2.txt')
+    assert 8 == day08b('test_input.txt')
 
 
 if __name__ == '__main__':
     test08a()
     print('Day 08a:', day08a('day08_input.txt'))
-    # test08b()
-    # print('Day 08b:', day08b('day08_input.txt'))
+    test08b()
+    print('Day 08b:', day08b('day08_input.txt'))
