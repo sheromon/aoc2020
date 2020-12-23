9467b844ba726aa0a61b5595f48b3fe5e6d01522
Cheryl Danner <sheromon@users.noreply.github.com>
2020-12-22 19:45:04 -0500

    Reverted to lists instead of queues because it seemed easier for part 2, and finished day22.

--- day22/day22.py
+++ day22/day22.py
@@ -1,11 +1,30 @@
-import queue
+import copy
 
 
 def day22a(input_path):
+    decks = parse_input(input_path)
+    round_loser = 1  # arbitrary value to start the loop
+    cards = {1: None, 2: None}
+    while decks[round_loser]:
+        for player in [1, 2]:
+            cards[player] = decks[player].pop()
+        if cards[1] > cards[2]:
+            round_winner = 1
+            round_loser = 2
+        else:
+            round_winner = 2
+            round_loser = 1
+        decks[round_winner] = [cards[round_loser], cards[round_winner]] + \
+            decks[round_winner]
+
+    return calc_score(decks[round_winner])
+
+
+def parse_input(input_path):
     lines = [line.strip() for line in open(input_path)]
     decks = {
-        1: queue.Queue(),
-        2: queue.Queue(),
+        1: [],
+        2: [],
     }
     player = None
     for line in lines:
@@ -16,32 +35,16 @@ def day22a(input_path):
         elif line == 'Player 2:':
             player = 2
         else:
-            decks[player].put(int(line))
-
-    done = False
-    cards = {1: None, 2: None}
-    while not done:
-        for player in [1, 2]:
-            cards[player] = decks[player].get()
-        if cards[1] > cards[2]:
-            round_winner = 1
-            round_loser = 2
-        else:
-            round_winner = 2
-            round_loser = 1
-        decks[round_winner].put(cards[round_winner])
-        decks[round_winner].put(cards[round_loser])
-        done = decks[round_loser].empty()
-
-    return calc_score(decks[round_winner])
+            decks[player].append(int(line))
+    # reverse the order of the cards so that pop() will return the next card
+    for player in decks.keys():
+        decks[player] = decks[player][::-1]
+    return decks
 
 
 def calc_score(deck):
-    all_cards = []
-    while not deck.empty():
-        all_cards.append(deck.get())
     score = 0
-    for ind, card in enumerate(all_cards[::-1]):
+    for ind, card in enumerate(deck):
         score += (ind + 1) * card
     return score
 
@@ -51,15 +54,51 @@ def test22a():
 
 
 def day22b(input_path):
-    pass
+    decks = parse_input(input_path)
+    winner, loser = recurse(decks)
+    return calc_score(decks[winner])
 
 
-# def test22b():
-#     assert 286 == day22b('test_input.txt')
+def recurse(decks, level=0):
+    history = []
+    round_loser = 1  # arbitrary value to start the loop
+    cards = {1: None, 2: None}
+    while decks[round_loser]:
+        state = tuple([tuple(deck) for deck in decks.values()])
+        if state in history:
+            round_winner = 1
+            round_loser = 2
+            break
+        history.append(state)
+        recurse_ok = True
+        for player in [1, 2]:
+            cards[player] = decks[player].pop()
+            recurse_ok = recurse_ok and (len(decks[player]) >= cards[player])
+        if recurse_ok:
+            decks_copy = {player: copy.deepcopy(deck[-cards[player]:])
+                     for player, deck in decks.items()}
+            round_winner, round_loser = recurse(decks_copy, level + 1)
+        else:
+            if cards[1] > cards[2]:
+                round_winner = 1
+                round_loser = 2
+            else:
+                round_winner = 2
+                round_loser = 1
+        # print('Player {} wins\n'.format(round_winner))
+        decks[round_winner] = [cards[round_loser], cards[round_winner]] + \
+            decks[round_winner]
+    return round_winner, round_loser
+
+
+def test22b():
+    assert 291 == day22b('test_input.txt')
+    decks = parse_input('test_input2.txt')
+    winner, loser = recurse(decks)
 
 
 if __name__ == '__main__':
     test22a()
     print('Day 22a:', day22a('day22_input.txt'))
-    # test22b()
-    # print('Day 22b:', day22b('day22_input.txt'))
+    test22b()
+    print('Day 22b:', day22b('day22_input.txt'))
